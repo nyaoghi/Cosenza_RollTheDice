98a4f71b94f177ed38a491d3bbe638dc9f921db0
Cosenza Olga <olgacosenza06@gmail.com>
2025-06-05 10:40:15 +0200

    push 5

--- app/src/main/java/com/example/cosenza_rolldice/ThirdActivity.kt
+++ app/src/main/java/com/example/cosenza_rolldice/ThirdActivity.kt
@@ -15,7 +15,7 @@ class ThirdActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+            
         }
-
     }
 }
